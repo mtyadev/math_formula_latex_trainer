1904a31eb8b436535ae40b70294c8f8faa043621
Miguel Tya <migueltya@test.com>
2021-06-16 13:39:27 +0200

    Implementing User and Session Management.

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,25 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user
+from app.models import User
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash("Login requested for user {}, remember_me={}".format(
-            form.username.data, form.remember_me.data)
-        )
-        return redirect(url_for('index'))
-    return render_template("login.html", titöe="Sign In", form=form)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid Username or Password')
+            return redirect(url_for('index'))
+    return render_template("login.html", title="Sign In", form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route("/")
 @app.route("/index")
